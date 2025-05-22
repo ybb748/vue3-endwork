ad180a4a12bab08a289a8a4c6447e736288d4587
zenghaonan <92354350+ybb748@users.noreply.github.com>
2025-05-22 21:51:53 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router';
+
+
+
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped lang="scss">
+    header {
+      color: $priceColor
+    }
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+
+</script>
+
+<template>
+    我是home
+</template>
+
+<style scoped></style>
